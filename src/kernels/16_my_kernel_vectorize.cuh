4c913a627f34c8b2d3f3c02c94fe1d8b6511b98d
bro_SiN <bro.sin@qq.com>
2025-02-24 21:20:24 +0800

    修改数据加载部分，原则上规避了越界问题，但是还是不正确

--- src/kernels/16_my_kernel_vectorize.cuh
+++ src/kernels/16_my_kernel_vectorize.cuh
@@ -57,25 +57,146 @@ __global__ void mysgemmVectorize(
     // 外层循环，循环U次，每次计算一个小矩阵C_{m_in_j}（对于整个block来说，对于这个线程来说是计算这个小矩阵中的一个小分块）
     for (uint u = 0; u < U; u++)
     {
-        // 将这个block计算需要的As和Bs加载到共享内存中
-        for (uint loadOffset = 0; loadOffset < strideA; loadOffset++)
+
+        // 计算出当前线程当前循环需要加载的数据所在的位置
+        const uint _As_col_start = As_col * VECTOR_SIZE; // 在一个BM*BK矩阵中的列数的起始位置
+        const uint _global_A_col_start = u * BK + _As_col_start;
+        const uint _global_A_col_end = _global_A_col_start + VECTOR_SIZE;
+        // 如果这一列的所有数据都越界了
+        if (_global_A_col_start >= K)
         {
-            uint aIndex = (As_row + loadOffset) * K + As_col * VECTOR_SIZE;
-            // 获取As的第As_row行，第As_col列到第(As_col+3)列的数据
-            const float4 tmp = reinterpret_cast<const float4 *>(&A[aIndex])[0];
-            // 将数据转置后加载到共享内存中
-            As[(As_col * VECTOR_SIZE + 0) * BM + As_row + loadOffset] = tmp.x;
-            As[(As_col * VECTOR_SIZE + 1) * BM + As_row + loadOffset] = tmp.y;
-            As[(As_col * VECTOR_SIZE + 2) * BM + As_row + loadOffset] = tmp.z;
-            As[(As_col * VECTOR_SIZE + 3) * BM + As_row + loadOffset] = tmp.w;
+            // 全部设置为0
+            for (uint loadOffset = 0; loadOffset < BM; loadOffset += strideA)
+            {
+                for (uint _vec_index = 0; _vec_index < VECTOR_SIZE; _vec_index++)
+                {
+                    As[(_As_col_start + _vec_index) * BM + As_row + loadOffset] = 0;
+                }
+            }
         }
-        for (uint loadOffset = 0; loadOffset < strideB; loadOffset++)
+        // 这一列有1或2或3个数据没有越界
+        else if (_global_A_col_end >= K)
         {
-            uint bIndex = (Bs_row + loadOffset) * N + Bs_col * VECTOR_SIZE;
-            // 获取Bs的第Bs_row行，第Bs_col列到第(Bs_col+3)列的数据
-            reinterpret_cast<float4 *>(&Bs[(Bs_row + loadOffset) * BN + Bs_col * VECTOR_SIZE])[0] =
-                reinterpret_cast<const float4 *>(&B[bIndex])[0];
+            for (uint _vec_index = 0; _vec_index < VECTOR_SIZE; _vec_index++)
+            {
+                if (_global_A_col_start + _vec_index < K) // 如果这一列没有越界，就仍然加载数据
+                {
+                    for (uint loadOffset = 0; loadOffset < BM; loadOffset += strideA)
+                    {
+                        // 如果行越界了
+                        if (C_row_index_i * BM + As_row + loadOffset >= M)
+                        {
+
+                            As[(_As_col_start + _vec_index) * BM + As_row + loadOffset] = 0;
+                        }
+                        else
+                        {
+                            As[(_As_col_start + _vec_index) * BM + As_row + loadOffset] =
+                                A[(As_row + loadOffset) * K + _As_col_start + _vec_index];
+                        }
+                    }
+                }
+                else // 如果这一列越界了，就设置成0
+                {
+                    for (uint loadOffset = 0; loadOffset < BM; loadOffset += strideA)
+                    {
+                        As[(_As_col_start + _vec_index) * BM + As_row + loadOffset] = 0;
+                    }
+                }
+            }
         }
+        else if (_global_A_col_start < K) // 列完全没有越界
+        {
+            for (uint loadOffset = 0; loadOffset < BM; loadOffset += strideA)
+            {
+                if (C_row_index_i * BM + As_row + loadOffset >= M) // 如果行越界
+                {
+
+                    for (uint _vec_index = 0; _vec_index < VECTOR_SIZE; _vec_index++)
+                    {
+                        As[(_As_col_start + _vec_index) * BM + As_row + loadOffset] = 0;
+                    }
+                }
+                else
+                { // 如果行没有越界，那么就是正常情况
+                    const float4 tmp = reinterpret_cast<const float4 *>(&A[(As_row + loadOffset) * K + _As_col_start])[0];
+                    // 向量化加载，并转置，存到As中
+                    As[(_As_col_start + 0) * BM + As_row + loadOffset] = tmp.x;
+                    As[(_As_col_start + 1) * BM + As_row + loadOffset] = tmp.y;
+                    As[(_As_col_start + 2) * BM + As_row + loadOffset] = tmp.z;
+                    As[(_As_col_start + 3) * BM + As_row + loadOffset] = tmp.w;
+                }
+            }
+        }
+        else
+        {
+            // 不存在的情况
+            printf("error\n");
+        }
+
+        const uint _Bs_col_start = Bs_col * VECTOR_SIZE;
+        const uint _global_B_col_start = C_col_index_j * BN + _Bs_col_start;
+        const uint _global_B_col_end = _global_B_col_start + VECTOR_SIZE;
+
+        if (_global_B_col_start >= N) // 列全部越界
+        {
+            // 全部设置为0
+            for (uint loadOffset = 0; loadOffset < BK; loadOffset += strideB)
+            {
+                reinterpret_cast<float4 *>(&Bs[(Bs_row + loadOffset) * BN + _Bs_col_start])[0] = make_float4(0, 0, 0, 0);
+            }
+        }
+        else if (_global_B_col_end >= N) // 有一些没有越界
+        {
+
+            for (uint _vec_index = 0; _vec_index < VECTOR_SIZE; _vec_index++)
+            {
+                if (_global_B_col_start + _vec_index < N) // 没有越界的部分
+                {
+                    for (uint loadOffset = 0; loadOffset < BK; loadOffset += strideB)
+                    {
+                        if (u * BK + Bs_row + loadOffset >= K) // 行越界
+                        {
+                            Bs[(Bs_row + loadOffset) * BN + _Bs_col_start + _vec_index] = 0;
+                        }
+                        else
+                        {
+                            Bs[(Bs_row + loadOffset) * BN + _Bs_col_start + _vec_index] =
+                                B[(Bs_row + loadOffset) * N + _Bs_col_start + _vec_index];
+                        }
+                    }
+                }
+                else
+                { // 越界的部分
+                    for (uint loadOffset = 0; loadOffset < BK; loadOffset += strideB)
+                    {
+                        Bs[(Bs_row + loadOffset) * BN + _Bs_col_start + _vec_index] = 0;
+                    }
+                }
+            }
+        }
+        else if (_global_B_col_start < N) // 列完全没有越界
+        {
+            for (uint loadOffset = 0; loadOffset < BK; loadOffset += strideB)
+            {
+                if (u * BK + Bs_row + loadOffset >= K) // 行越界
+                {
+                    reinterpret_cast<float4 *>(&Bs[(Bs_row + loadOffset) * BN + _Bs_col_start])[0] = make_float4(0, 0, 0, 0);
+                }
+                else
+                {
+
+                    reinterpret_cast<float4 *>(&Bs[(Bs_row + loadOffset) * BN + _Bs_col_start])[0] =
+                        reinterpret_cast<const float4 *>(&B[(Bs_row + loadOffset) * N + _Bs_col_start])[0];
+                }
+            }
+        }
+        else
+        {
+            // 不存在的情况
+            printf("error\n");
+        }
+
         __syncthreads();
         A += BK;     // A向右移动BK
         B += BK * N; // B向下移动BK
@@ -114,8 +235,36 @@ __global__ void mysgemmVectorize(
     // 将计算结果写回去
     for (uint _res_row = 0; _res_row < TM; _res_row++)
     {
+        if (M % BM != 0)
+        {
+            if (C_row_index_i == CEIL_DIV(M, BM) - 1)
+            {
+                if (Cs_row_t * TM + _res_row >= M % BM)
+                {
+                    break;
+                }
+            }
+        }
+
         for (uint _res_col = 0; _res_col < TN; _res_col += VECTOR_SIZE)
         {
+            if (N % BN != 0)
+            {
+                if (C_col_index_j == CEIL_DIV(N, BN) - 1)
+                {
+                    for (uint _vec_index = 0; _vec_index < VECTOR_SIZE; _vec_index++)
+                    {
+                        if (Cs_col_s * TN + _res_col + _vec_index < N % BN)
+                        {
+                            float _tmp = C[(Cs_row_t * TM + _res_row) * N + Cs_col_s * TN + _res_col + _vec_index];
+                            C[(Cs_row_t * TM + _res_row) * N + Cs_col_s * TN + _res_col + _vec_index] = alpha * Cs_m_t_n_s[_res_row * TN + _res_col + _vec_index] + beta * _tmp;
+                        }
+                    }
+                    // 最后一个列分块单独处理（不能用向量去做，会越界）
+                    break;
+                }
+            }
+
             float4 tmp = reinterpret_cast<float4 *>(&C[(Cs_row_t * TM + _res_row) * N + Cs_col_s * TN + _res_col])[0];
             tmp.x = alpha * Cs_m_t_n_s[_res_row * TN + _res_col] + beta * tmp.x;
             tmp.y = alpha * Cs_m_t_n_s[_res_row * TN + _res_col + 1] + beta * tmp.y;
